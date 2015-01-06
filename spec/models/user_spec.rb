b27abb96e8d2004a9ab78fcb1e63c68fa1b81bf9
Steven Doll <steven@rigil.com>
2015-01-06 09:17:23 -0500

    enhanced user model with avatar, name, uuid, rspec tests [finishes #85512752]

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,37 @@ describe User do
 
   it { should respond_to(:email) }
 
-  it "#email returns a string" do
+  it "85512752 #email returns a string" do
     expect(@user.email).to match 'user@example.com'
   end
 
+  # it "is invalid without a password" do 
+  #   user = User.new(password: nil) 
+  #   user.valid? 
+  #   expect(user.errors[:password]).to include("can't be blank") 
+  # end
+
+  it "85512752 is invalid without an email" do 
+    user = User.new(email: nil) 
+    user.valid? 
+    expect(user.errors[:email]).to include("can't be blank") 
+  end
+
+  it "85512752 is invalid with a duplicate email address" do
+    User.create(
+      first_name: 'Joe', last_name: 'Tester',
+      email: 'tester@example.com'
+    )
+    user = User.new(
+     first_name: 'Jane', last_name: 'Tester',
+     email: 'tester@example.com'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+  it "85512752 returns a user's full name as a string" do
+    user = User.new(first_name: 'John', last_name: 'Doe', email: 'johndoe@example.com')
+    expect(user.full_name).to eq 'John Doe'
+  end
 end
