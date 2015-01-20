08948f0d96e08350ce62ed472ccaa972f3aa9c86
Steven Doll <steven@rigil.com>
2015-01-20 11:54:13 -0500

    changed fatsecret signin link

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
 
   get '/auth/fatsecret/callback', to: 'api_tokens#create'
-  
+  get '/auth/fatsecret'
+
+
   devise_for :users
   resources :users
 end
