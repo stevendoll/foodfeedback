ca21a6fc8f211687236dee682dd78adfd044a6c6
Steven Doll <steven@rigil.com>
2015-01-26 14:17:17 -0500

    fix user and account seed services

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
-
 account = CreateAccountService.new.call
 puts 'CREATED ADMIN ACCOUNT: ' << account.name
 
-user.account = account
-user.save
+user = CreateAdminService.new.call
+puts 'CREATED ADMIN USER: ' << user.email
+
 # Environment variables (ENV['...']) can be set in the file .env file.
 
 # puts 'ACCOUNTS'
