80427415495136c7f44358a15210b4d34b6f7404
Steven Doll <steven@rigil.com>
2015-02-13 20:24:49 -0500

    loop through fatsecret month records [finishes #85376414]

--- spec/features/practitioners/diet_review_spec.rb
+++ spec/features/practitioners/diet_review_spec.rb
@@ -66,7 +66,6 @@ feature 'Practioner Diet Review' do
     client = User.where(:account => practitioner.account).first
     login_as(client, scope: :user)
     visit practice_client_path(client)
-    expect(page).to have_content 'Welcome'
     expect(page).to have_content 'Access denied.'
   end
 
@@ -82,7 +81,6 @@ feature 'Practioner Diet Review' do
     other_client = User.where(:account => practitioner.account).last
     login_as(client, scope: :user)
     visit practice_client_path(other_client)
-    expect(page).to have_content 'Welcome'
     expect(page).to have_content 'Access denied.'
   end
 
